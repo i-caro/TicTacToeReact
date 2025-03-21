1a242569171e060232d47bce786a4947e9b35a1b
Iván <ivancaroromero31@gmail.com>
2025-03-21 15:13:01 +0100

    game board

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 
 
-export default function Player ({name, symbol}) {
+export default function Player ({name, symbol, isActive}) {
+    const [playerName, setPlayerName] = useState(name);
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick(){
         setIsEditing((editing) => !editing);
     }
 
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
     return (
-        <li>
+        <li className={isActive? 'active' : undefined}>
             <span className="player">
-              {isEditing ? <input type="text" required placeholder={name} /> : <span className="player-name">{name}</span> }
+              {isEditing ? <input type="text" required value={playerName} onChange={handleChange} /> : <span className="player-name">{playerName}</span> }
               <span className="player-symbol">{symbol}</span>
             </span>
             {isEditing? <button onClick={handleEditClick}>Save</button> : <button onClick={handleEditClick}>Edit</button>}
